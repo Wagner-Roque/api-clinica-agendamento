60e1c79275f2e3db7706d084825481d7e31c2ded
Roque <roquewagnertadeu@gmail.com>
2025-01-28 15:30:12 -0300

    first commit

---
+++ src/main/java/com/roque/voll/api_clinica_medica/ApiClinicaMedicaApplication.java
@@ -0,0 +1,13 @@
+package com.roque.voll.api_clinica_medica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClinicaMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClinicaMedicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roque/voll/api_clinica_medica/ApiClinicaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roque.voll.api_clinica_medica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClinicaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
